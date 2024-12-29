5979a46cfb3918935b8925b76346769eb429002d
teterichard <teterichard.dias@gmail.com>
2024-12-29 20:33:13 -0300

    deploy2

--- next.config.js
+++ next.config.js
@@ -7,4 +7,10 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {};
 
+module.exports = {
+    images: {
+      domains: ['proeng-5r0c5koj2-proengs-projects.vercel.app'],
+    },
+  };
+
 export default config;
