bacfe65b0a31922144b0f4b719349e39939a7942
teterichard <teterichard.dias@gmail.com>
2025-01-10 15:31:32 -0300

    atual15

--- src/components/numeros.tsx
+++ src/components/numeros.tsx
@@ -12,23 +12,27 @@ export default function Numero({ n }: NumeroProps) {
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
-        if (entry?.isIntersecting) { // Usando optional chaining
+        if (entry?.isIntersecting) {
           setStartAnimation(true);
         }
       },
       { threshold: 0.5 } // Define que a animação começa quando 50% do elemento está visível
     );
 
-    ref.current && observer.observe(ref.current); // Usando && ao invés de if para ser mais conciso
+    if (ref.current) {
+      observer.observe(ref.current); // Usa `if` explícito para observar
+    }
 
     return () => {
-      ref.current && observer.unobserve(ref.current); // Usando && novamente
+      if (ref.current) {
+        observer.unobserve(ref.current); // Usa `if` explícito para desobservar
+      }
     };
   }, []);
 
   const { numero } = useSpring({
     from: { numero: 0 },
-    to: { numero: startAnimation ? n : 0 }, // Inicia a animação quando `startAnimation` for verdadeiro
+    to: { numero: startAnimation ? n : 0 }, // Inicia a animação quando startAnimation for verdadeiro
     config: { mass: 1, tension: 20, friction: 10 },
     delay: 200,
   });
