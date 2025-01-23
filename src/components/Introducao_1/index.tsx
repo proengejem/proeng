860c341d8f6f827619ef8e900945d0d7e973ab0c
LucasZanini096 <lucas608@outlook.com>
2025-01-23 10:26:15 -0300

    fix: rendering text in Introducao_1

--- src/components/Introducao_1/index.tsx
+++ src/components/Introducao_1/index.tsx
@@ -25,30 +25,30 @@ const HeroSectionWithVideo: React.FC = () => {
       <div className="relative z-20 flex flex-col items-center justify-center h-full text-center text-white px-4">
         <div className="max-w-2xl">
           {/* Título */}
-          <BlurFade delay={0.3}>
+          <div className="mb-4">
             <h1
               className="text-4xl md:text-6xl font-bold leading-tight"
               style={{ color: "#027A48" }}
             >
               PROENG GEOTECNIA{" "}
             </h1>
-          </BlurFade>
-          <BlurFade delay={0.3}>
+          </div>
+          <div className="mb-4">
             <h1 className="text-4xl md:text-6xl font-bold leading-tight">
               Construindo qualidade e confiança desde{" "}
               <span style={{ color: "#027A48" }}>2006</span>
             </h1>
-          </BlurFade>
+          </div>
           {/* Descrição */}
-          <BlurFade delay={0.5}>
+          <div className="mb-4">
             <p className="mt-2 text-base md:text-lg">
               A PROENG possui uma trajetória sólida no mercado de engenharia,
               oferecemos soluções inovadoras e confiáveis. Nossa missão é
               garantir a satisfação em cada projeto.
             </p>
-          </BlurFade>
+          </div>
           {/* Botões */}
-          <BlurFade delay={0.6}>
+          <div className="mt-6 flex space-x-4 justify-center">
             <div className="mt-6 flex space-x-4 justify-center">
               <Link href="/sobrenos">
                 <button className="px-6 py-3 bg-[#027A48] text-white rounded-lg font-semibold hover:bg-green-500 transition">
@@ -64,7 +64,7 @@ const HeroSectionWithVideo: React.FC = () => {
                 </button>
               </Link>
             </div>
-          </BlurFade>
+          </div>
         </div>
       </div>
 
