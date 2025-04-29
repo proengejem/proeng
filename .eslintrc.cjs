396bbe3628c3a382b7eb8ff10bd28973604393b4
LucasZanini096 <lucas608@outlook.com>
2025-04-29 19:16:53 -0300

    feat: ajusts Eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,8 @@ const config = {
   "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-arguments": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/consistent-type-imports": [
