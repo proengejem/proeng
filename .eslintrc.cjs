e7a49aa649a7b1af1bcd67b673cba284cf46b7d7
LucasZanini096 <lucas608@outlook.com>
2025-04-30 14:06:21 -0300

    fix: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,8 @@ const config = {
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
